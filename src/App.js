048ff7dbd9ee2ea2fe9129563d68c9290225887c
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2022-04-12 06:24:55 -0300

    19. Componentes fáciles de extender o de propiedades dinamicas

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import "./App.css";
 // COMPONENTS
 import Home from "./components/Home";
 import Header from "./components/Header/Header.js";
-import Paragraph from './components/Paragraph/Paragraph.js';
+import Paragraph from "./components/Paragraph/Paragraph.js";
+import ParagraphDinamicProps from "./components/Paragraph-dinamic-prop/Paragraph-dinamic-prop.js";
 
 class App extends Component {
   state = {
     miau: "bienvenido a Miau",
-  }
+  };
 
   cambiarTextoDelEstado = () => {
     this.setState({
       miau: "Hola Mundo",
     });
-  }
+  };
 
   render() {
     const { miau } = this.state;
-    const texto = "Hola Gatos";
+    const texto = "texto texto";
     const clickHandler = (text) => {
       console.log(text);
     };
@@ -30,30 +31,19 @@ class App extends Component {
     };
     return (
       <div className="App">
-        {/**
-         * **Header** is an example of an stateLess component
-         */}
+
         <Header miau={texto} manejaClick={clickHandler} homeLink="Home" />
         <Header miau={miau} manejaClick={clickHandler} homeLink="Home" />
-        <Paragraph>
-          {miau}
-        </Paragraph>
+
         <p onClick={this.cambiarTextoDelEstado} className="App-intro">
           {miau}
         </p>
-        {/**
-         * _Acerca de lo elemento hijos de un componente_
-         * los componentes pueden utilizarse como un cierre en si mismos,
-         * eg: *<Home />*
-         * o tambien como elementos tag de apertura y cierre, los cuales pueden incluir
-         * otros elementos html, o componentes
-         * estos seran recibidos en en la clase en el objeto _this.props.children_
-         * eg:
-         *    *<Home>*
-         *      *<p>soy hijo de home</p>*
-         *    *</Home>*
-         */}
-        {/* <Home name={"Chaboncito"} initialAge={34} /> */}
+
+        <Paragraph>{miau}</Paragraph>
+
+        <ParagraphDinamicProps onClick={this.cambiarTextoDelEstado}>
+          {texto}
+        </ParagraphDinamicProps>
       </div>
     );
   }
