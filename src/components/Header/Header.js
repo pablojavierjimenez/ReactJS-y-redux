0084b7c7b88e8aa74ef2587f3d03d3f35cc88bf7
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2022-04-14 14:46:18 -0300

    20. CSS vs Inline Style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,25 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from "react";
+import logo from "../../logo.svg";
 
-
+/**
+ * @description Definiendo estilos en linea encapsulados dentro de el mismo componente
+ * @example Nº1:
+ *    en este ejemplo definimos las propiedades de css dentro de un objeto
+ *    estatico el cual luego pasamos como variable al atributo 'style={}'
+ *
+ */
+const styles = {
+  header: {
+    backgroundColor: "#282c34",
+    minHeight: "10vh",
+    display: "flex",
+    flexDirection: 'column',
+    alignItems: "center",
+    justifyContent: "space-around",
+    fontSize: "calc(10px + 1vmin)",
+    color: "white",
+  },
+};
 /**
  * STATELESS component
  * in this kind of component the principal difference is
@@ -10,16 +28,18 @@ import logo from '../../logo.svg';
  */
 export default class Header extends Component {
   updateProps = () => {
-    const {miau, manejaClick} = this.props;
+    const { miau, manejaClick } = this.props;
     manejaClick(miau);
-  }
+  };
   render() {
-    const {miau, manejaClick} = this.props;
+    const { miau, manejaClick } = this.props;
 
     return (
-      <div className="App-header">
+      <div style={styles.header}>
         <img src={logo} className="App-logo" alt="logo" />
-        <h2 onClick={this.updateProps} className='App-title'>{miau}</h2>
+        <h2 onClick={this.updateProps} className="App-title">
+          {miau}
+        </h2>
       </div>
     );
   }
