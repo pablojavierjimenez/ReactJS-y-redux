a18aa1db1adb566f9b2d02a671ee7e50b33d24a2
Pablo Javier Jimenez <pablojavierjimenez@gmail.com>
2022-04-16 06:39:46 -0300

    24. Validaciones de errores de los formularios

--- src/components/FormularioSimple/FormularioSimple.js
+++ src/components/FormularioSimple/FormularioSimple.js
@@ -1,27 +1,58 @@
-import React, { Component  } from 'react';
+import React, { Component } from "react";
 
 export default class FormularioSimple extends Component {
-  state = {};
+  state = {
+    errors: {},
+    formData: {},
+  };
 
   handleChange = (event) => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     this.setState({
-      [name]: value
+      formData: {
+        [name]: value,
+      },
     });
-  }
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log('enviado', this.state);
-  }
+    const { nombre, apellidos } = this.state;
+    if(this.validate(this.state.formData)) {
+      console.log("enviado", this.state.formData);
+    } else {
+      console.log("Hay campos con errores", this.state.errors);
+    }
+  };
+
+  validate({ nombre, apellidos }) {
+    const errors = {};
+    if (!nombre) errors.nombre = "El nombre es obligatorio";
+    if (!apellidos) errors.apellidos = "El apellido es obligatorio";
+    this.setState({ errors });
+    return (Object.keys(errors).length === 0) ? true : false;
+  };
 
   render() {
-    return(
-    <form onSubmit={this.handleSubmit}>
-      <input name='nombre' onChange={this.handleChange} type="text" placeholder="Nombre" />
-      <input name='apellidos' onChange={this.handleChange} type="text" placeholder="Apellido" />
-      <input type="submit" value="Enviar" />
-    </form>
-    )
+    const { errors } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {errors.nombre && <p>{errors.nombre}</p>}
+        <input
+          name="nombre"
+          onChange={this.handleChange}
+          type="text"
+          placeholder="Nombre"
+          />
+          {errors.apellidos && <p>{errors.apellidos}</p>}
+        <input
+          name="apellidos"
+          onChange={this.handleChange}
+          type="text"
+          placeholder="Apellido"
+        />
+        <input type="submit" value="Enviar" />
+      </form>
+    );
   }
-}
+};
