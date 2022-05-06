eae1917f70015951d5516be023cab8dce8fc071d
pablo javier jimenez <pablojavierjimenez@gmail.com>
2022-05-06 09:14:02 -0300

    38. Validando los campos del formulario

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,8 +1,25 @@
 import React, {Component } from "react";
 import { act } from "react-dom/test-utils";
 
+const validate = values =>{
+  console.log('sin errors',values);
+  const errors = {};
+  if (!values.name){
+    errors.name = 'Nombre campo es obligatorio'
+  }
+  if (!values.email){
+    errors.email = 'Email campo es obligatorio'
+  }
+  if (!values.website){
+    errors.website = 'Website campo es obligatorio'
+  }
+  return errors;
+}
+
 export default class UserForm extends Component {
-  state = {};
+  state = {
+    errors:{}
+  };
 
   handleChange = ({target}) => {
     this.setState({
@@ -12,18 +29,38 @@ export default class UserForm extends Component {
   }
 
   onSubmitForm = (e) => {
+    // console.log(e.target);
     e.preventDefault();
+    const {errors, ...sinErrors} = this.state;
+    const result = validate(sinErrors);
+    this.setState({
+      errors: result
+    })
+    if (!Object.keys(result).length) {
+      // envio
+      e.target.reset();
+    }
     console.log(this.state);
   }
   render() {
     const { actualUser } = this.props;
+    const {errors} = this.state;
     return (
-      <form>
+      <form onSubmit={this.onSubmitForm}>
         <h2>User Form {actualUser}</h2>
-        <input name="name" type="text" placeholder="Nombre" onChange={this.handleChange}/>
-        <input name="email" type="email" placeholder="email" onChange={this.handleChange}/>
-        <input name="website" type="text" placeholder="website" onChange={this.handleChange}/>
-        <input name="submit" type="submit" value="send new User" onClick={this.onSubmitForm}/>
+        <div>
+          <input name="name" type="text" placeholder="Nombre" onChange={this.handleChange}/><br/>
+          {errors.name && <small><mark>{errors.name}</mark></small>}
+        </div>
+        <div>
+          <input name="email" type="email" placeholder="email" onChange={this.handleChange}/><br/>
+          {errors.email && <small><mark>{errors.email}</mark></small>}
+        </div>
+        <div>
+          <input name="website" type="text" placeholder="website" onChange={this.handleChange}/><br/>
+          {errors.website && <small><mark>{errors.website}</mark></small>}
+        </div>
+        <input name="submit" type="submit" value="send new User"/>
       </form>
     );
   }
