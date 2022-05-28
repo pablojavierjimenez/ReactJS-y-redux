d13b476a1194ce7358d36c8130f586460ad27a70
pablo javier jimenez <pablojavierjimenez@gmail.com>
2022-05-28 07:29:39 -0300

    51. Action Creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,21 @@
 const initialState = 0;
 const INCREMENTAR = 'CONTADOR/INCREMENTAR';
 const DISMINUIR = 'CONTADOR/DISMINUIR';
+const SETEAR = 'CONTADOR/SETEAR';
+
+// ACTION CREATORS
+const incrementar = () => ({
+    type: INCREMENTAR,
+});
+const disminuir = () => ({
+    type: DISMINUIR,
+});
+
+const setear = (payload) => ({
+    type: DISMINUIR,
+    payload,
+});
+
 export default function reducer(state = initialState, action) {
     switch(action.type) {
         case INCREMENTAR:
@@ -8,6 +23,9 @@ export default function reducer(state = initialState, action) {
             break;
         case DISMINUIR:
             return state - 1;
+            break;
+        case SETEAR: 
+            return action.payload;
             break;
         default:
             return state;
